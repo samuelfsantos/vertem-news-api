f5363e85e9e0f137120eb5ac8c2e84b3dc0651b4
Samuel Ferreira Santos <fs.samuel@outlook.com>
2024-03-14 03:52:41 -0300

    ajustes pipeline

--- Vertem.News/Vertem.News.Api/Configurations/CacheConfigurationExtensions.cs
+++ Vertem.News/Vertem.News.Api/Configurations/CacheConfigurationExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
 
 namespace Vertem.News.Api.Configurations
 {
@@ -6,13 +8,15 @@ namespace Vertem.News.Api.Configurations
     {
         public static void AddCacheConfig(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment env)
         {
-            services.AddDistributedMemoryCache();
+            //services.AddDistributedMemoryCache();
 
             services.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = configuration.GetConnectionString("Cache");
                 options.InstanceName = $"CacheOficinaTech{env.EnvironmentName}";
             });
+
+            services.Add(ServiceDescriptor.Singleton<IDistributedCache, RedisCache>());
         }
     }
 }
