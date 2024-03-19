f5ed4b3c5b13586150d9073216d93d0d1c33fa5c
Samuel Ferreira Santos <fs.samuel@outlook.com>
2024-03-19 01:40:15 -0300

    ajustes noticia output

--- Vertem.News/Vertem.News.Domain/Outputs/NoticiaOutput.cs
+++ Vertem.News/Vertem.News.Domain/Outputs/NoticiaOutput.cs
@@ -5,14 +5,14 @@ namespace Vertem.News.Domain.Outputs
 {
     public class NoticiaOutput : BaseOutput
     {
-        public string Titulo { get; private set; }
-        public string Descricao { get; private set; }
-        public string Conteudo { get; private set; }
-        public string Categoria { get; private set; }
-        public string Fonte { get; private set; }
-        public DateTime DataPublicacao { get; private set; }
-        public string? ImgUrl { get; private set; }
-        public string? Autor { get; private set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Conteudo { get; set; }
+        public string Categoria { get; set; }
+        public string Fonte { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        public string? ImgUrl { get; set; }
+        public string? Autor { get; set; }
 
         public NoticiaOutput(
             Guid id,
