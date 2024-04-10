f452f43304fe3c99698dca11b922c86c922fbdb0
Samuel Ferreira Santos <fs.samuel@outlook.com>
2024-04-10 01:38:16 -0300

    ajustes log

--- Vertem.News/Vertem.News.Api/Configurations/LogConfigurationExtensions.cs
+++ Vertem.News/Vertem.News.Api/Configurations/LogConfigurationExtensions.cs
@@ -9,28 +9,28 @@ namespace Vertem.News.Api.Configurations
         public static void AddLogConfig(this WebApplicationBuilder builder, IConfiguration configuration)
         {
             builder.Logging.ClearProviders();
-            //var logger = new LoggerConfiguration()
-            //    .MinimumLevel.Error()
-            //    .Enrich.WithProperty("Version", Assembly.GetEntryAssembly()!.GetName().Version)
-            //    .Enrich.WithEnvironmentName()
-            //    .Enrich.WithMachineName()
-            //    .Enrich.WithProcessId()
-            //    .Enrich.WithThreadId()
-            //    .Enrich.WithMemoryUsage()
-            //    .Enrich.FromLogContext()
-            //    .WriteTo.Console()
-            //    .WriteTo.Seq("http://localhost:5012/")
-            //    .CreateLogger();
-            //builder.Logging.AddSerilog(logger);
-
-            builder.Host.UseSerilog((context, loggerConfig) => {
-                loggerConfig
-                .ReadFrom.Configuration(context.Configuration)
-                .WriteTo.Console()
-                .Enrich.WithExceptionDetails()
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Error()
+                .Enrich.WithProperty("Version", Assembly.GetEntryAssembly()!.GetName().Version)
+                .Enrich.WithEnvironmentName()
+                .Enrich.WithMachineName()
+                .Enrich.WithProcessId()
+                .Enrich.WithThreadId()
+                .Enrich.WithMemoryUsage()
                 .Enrich.FromLogContext()
-                .WriteTo.Seq("vertem-news-seq:5012");
-            });
+                .WriteTo.Console()
+                .WriteTo.Seq("vertem-news-seq:5012")
+                .CreateLogger();
+            builder.Logging.AddSerilog(logger);
+
+            //builder.Host.UseSerilog((context, loggerConfig) => {
+            //    loggerConfig
+            //    .ReadFrom.Configuration(context.Configuration)
+            //    .WriteTo.Console()
+            //    .Enrich.WithExceptionDetails()
+            //    .Enrich.FromLogContext()
+            //    .WriteTo.Seq("vertem-news-seq:5012");
+            //});
         }
 
         //public static void UseLogConfig(this WebApplication app)
