70b4ed13e49ae2681d9e872871fcb3a156a25c8f
Samuel Ferreira Santos <fs.samuel@outlook.com>
2024-04-10 01:16:58 -0300

    ajuste log

--- Vertem.News/Vertem.News.Api/Configurations/LogConfigurationExtensions.cs
+++ Vertem.News/Vertem.News.Api/Configurations/LogConfigurationExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Serilog;
 using System.Reflection;
+using Serilog.Exceptions;
 
 namespace Vertem.News.Api.Configurations
 {
@@ -8,19 +9,37 @@ namespace Vertem.News.Api.Configurations
         public static void AddLogConfig(this WebApplicationBuilder builder, IConfiguration configuration)
         {
             builder.Logging.ClearProviders();
-            var logger = new LoggerConfiguration()
-                .MinimumLevel.Error()
-                .Enrich.WithProperty("Version", Assembly.GetEntryAssembly()!.GetName().Version)
-                .Enrich.WithEnvironmentName()
-                .Enrich.WithMachineName()
-                .Enrich.WithProcessId()
-                .Enrich.WithThreadId()
-                .Enrich.WithMemoryUsage()
-                .Enrich.FromLogContext()
+            //var logger = new LoggerConfiguration()
+            //    .MinimumLevel.Error()
+            //    .Enrich.WithProperty("Version", Assembly.GetEntryAssembly()!.GetName().Version)
+            //    .Enrich.WithEnvironmentName()
+            //    .Enrich.WithMachineName()
+            //    .Enrich.WithProcessId()
+            //    .Enrich.WithThreadId()
+            //    .Enrich.WithMemoryUsage()
+            //    .Enrich.FromLogContext()
+            //    .WriteTo.Console()
+            //    .WriteTo.Seq("http://localhost:5012/")
+            //    .CreateLogger();
+            //builder.Logging.AddSerilog(logger);
+
+            builder.Host.UseSerilog((context, loggerConfig) => {
+                loggerConfig
+                .ReadFrom.Configuration(context.Configuration)
                 .WriteTo.Console()
-                .WriteTo.Seq("http://localhost:5012/")
-                .CreateLogger();
-            builder.Logging.AddSerilog(logger);
+                .Enrich.WithExceptionDetails()
+                .Enrich.FromLogContext()
+                .WriteTo.Seq("http://localhost:5012");
+            });
         }
+
+        //public static void UseLogConfig(this WebApplication app)
+        //{
+        //    app.UseSerilogRequestLogging(options =>
+        //    {
+        //        options.EnrichDiagnosticContext = Enricher.HttpRequestEnricher;
+        //    });
+        //}
+
     }
 }
